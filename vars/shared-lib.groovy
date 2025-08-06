7e75a58998713ad8f91df6852b58c7346e94f75f
Your Name <vemasani.pavani@gmail.com>
2025-08-06 17:41:35 +0530

    check out the code in scripted pipeline

---
+++ vars/shared-lib.groovy
@@ -0,0 +1,47 @@
+def call()
+{
+    node {
+
+        stage('code checkout') {
+            sh 'env'
+            echo "Code checkout"
+            if (env.tag_name ==~ '.*') {
+                checkout([$class           : 'GitSCM',
+                          branches         : [[name: "refs/tags/${env.tag_name}"]],
+                          userRemoteConfigs: [[url: 'https://github.com/pdevops78/expense-backend.git']]
+                ])
+            } else {
+                checkout([$class           : 'GitSCM',
+                          branches         : [[name: "*/${env.branch_name}"]],
+                          userRemoteConfigs: [[url: 'https://github.com/pdevops78/expense-backend.git']]
+                ])
+            }
+        }
+        if (env.tag_name ==~ '.*') {
+            stage('Build Code') {
+                echo "OK"
+
+            }
+            stage('Release Software') {
+                echo "OK"
+            }
+        } else {
+            stage('LintCode') {
+                sh 'env'
+                echo "OK"
+            }
+            if (env.branch_name != 'main') {
+                stage('Unit test') {
+                    echo "OK"
+                }
+                stage('Integration test') {
+                    echo "OK"
+                }
+            } else {
+                stage('code review') {
+                    echo "OK"
+                }
+            }
+        }
+    }
+}
