515450a28e059b8a0ec6d7d97ac35e2ee85ff706
Your Name <vemasani.pavani@gmail.com>
2025-09-04 13:28:39 +0530

    roboshop jenkins

--- vars/sharedLib.groovy
+++ vars/sharedLib.groovy
@@ -18,13 +18,26 @@ def call()
             }
         }
         if (env.tag_name ==~ '.*') {
+            sh 'docker build -t 041445559784.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME} .'
+            print 'OK'
             stage('Build Code') {
                 echo "OK"
 
             }
             stage('Release Software') {
                 echo "OK"
+                sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 041445559784.dkr.ecr.us-east-1.amazonaws.com'
+                sh 'docker push 041445559784.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME}'
             }
+            stage('Deploy to Dev'){
+                sh 'aws eks update-kubeconfig --name dev-eks'
+                sh 'argocd login argocd-dev.pdevops78.online --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --skip-test-tls --grpc-web'
+                sh 'argocd app create ${component} --repo https://github.com/devps23/eks-helm-argocd-ingress.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --insecure  --grpc-web --values values/${component}.yaml'
+                sh 'argocd app set ${component} --parameter appVersion=${TAG_NAME}'
+                sh 'argocd app sync ${component}'
+                print 'OK'
+            }
+
         } else {
             stage('LintCode') {
                 sh 'env'
